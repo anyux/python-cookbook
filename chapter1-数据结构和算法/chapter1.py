d4b53846fabd017f5e4dfe3a715cc1e66951934a
gaochao <1915530614@qq.com>
2024-09-28 17:56:49 +0800

    保存最后N个变量

--- chapter1-数据结构和算法/chapter1.py
+++ chapter1-数据结构和算法/chapter1.py
@@ -1,9 +1,16 @@
-items=[
-    1,10,7,4,5,9
-]
+from collections import deque
 
-head,*tail=items
+def search(lines,pattern,histroy=5):
+    previous_line= deque(maxlen=histroy)
+    for line in lines:
+        if pattern in line:
+            yield line,previous_line
+        previous_line.append(line)
 
-print(head)
-
-print(tail)
+if __name__ == '__main__':
+    with open('input.txt','r') as f:
+        for line,previous_line in search(f,'python',5):
+            for pline in previous_line:
+                print(pline,end='')
+            print(line,end='')
+            print('-'*20)
